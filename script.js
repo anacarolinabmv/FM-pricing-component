192a0d16cf23c341e4279290a496e868f2a29f9e
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-10-16 19:49:42 +0200

    Refactor JS logic

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const toggleBtn = document.querySelector('[type="checkbox"]');
 const pricesMonthly = document.querySelectorAll('.price--monthly');
 const pricesAnnually = document.querySelectorAll('.price--annually');
 
-toggleBtn.addEventListener('click', () => {
-  if (toggleBtn.checked) {
-    pricesMonthly.forEach((price) => price.classList.remove('hide'));
-    pricesAnnually.forEach((price) => price.classList.add('hide'));
-  } else {
-    pricesMonthly.forEach((price) => price.classList.add('hide'));
-    pricesAnnually.forEach((price) => price.classList.remove('hide'));
-  }
-});
+const togglePrices = () => {
+  const hideClass = 'hide';
+
+  pricesMonthly.forEach((price) => {
+    price.classList.toggle(hideClass, !toggleBtn.checked);
+  });
+
+  pricesAnnually.forEach((price) => {
+    price.classList.toggle(hideClass, toggleBtn.checked);
+  });
+};
+
+toggleBtn.addEventListener('click', togglePrices);
